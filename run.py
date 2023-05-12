ce42a2f9bbf022b6d494631935c5800296c51a8f
phillywhitty <philipwhitty92@yahoo.ie>
2023-05-12 12:15:38 +0000

    add basic structure for display_instruction function

--- run.py
+++ run.py
@@ -12,19 +12,25 @@ username()
 
 
 
+
+
+
 user_choice = ""
 
 def new_game():
     """
     Prints start game/instructions message and validates input for game to start.
     """
-
     user_choice = input("Type 'p' to start game or 'r' to read the rules!\n>")
-if user_choice == "r":
-    print("display instructions")
-elif user_choice == "p":
-    print("set_board")   
+    if user_choice == "r":
+        display_instructions()
+    elif user_choice == "p":
+        print("set_board")   
 
+def display_instructions():
+    """
+    Prints instructions to the screen for the user when "r" is selected.
+    """
+    print("These are the rules of my game")
 
-
-new_game()     
+new_game()
