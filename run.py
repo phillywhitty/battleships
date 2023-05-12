adc0bffdabb91608e4f9c96da4da6726a5a618fd
phillywhitty <philipwhitty92@yahoo.ie>
2023-05-12 12:03:43 +0000

    add new game functions basic structure

--- run.py
+++ run.py
@@ -5,7 +5,26 @@ def username():
     Gets the username
     """
     user = input('Enter your username: ')
-    print('Welcome to the battle of the ships{user}!')
+    print(f'Welcome to the battle of the ships {user}!')
 
 
 username()
+
+
+
+user_choice = ""
+
+def new_game():
+    """
+    Prints start game/instructions message and validates input for game to start.
+    """
+
+    user_choice = input("Type 'p' to start game or 'r' to read the rules!\n>")
+if user_choice == "r":
+    print("display instructions")
+elif user_choice == "p":
+    print("set_board")   
+
+
+
+new_game()     
