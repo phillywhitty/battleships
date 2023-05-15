58adec2babf8407a1a02e86b7ad92296b285b5ff
phillywhitty <philipwhitty92@yahoo.ie>
2023-05-15 09:08:34 +0000

    add get_shot & check_shot functions. create loops to check users guesses

--- run.py
+++ run.py
@@ -1,50 +1,27 @@
-from pprint import pprint
 
-BOARD_SIZE = 5
-player_board = []
-computer_board = []
+def get_shot(guesses):
 
+    ok = "n"
+    while ok =="n":
+        try:
+            shot = input("please enter your guess")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("incorrect number, please try again")
+            elif shot in guesses:  
+                print("incorrect number, used before")  
+            else:
+                ok = "y"
+                break 
+        except:
+            print("incorrect entry - please enter again")
 
-
-
-
-def get_username():
-    """
-    Gets the username
-    """
-    user = input('Enter your username: ')
-    print(f'Welcome to the battle of the ships {user}!')
-    return username
-
-
-
-
-user_choice = ""
-
-def new_game():
-    """
-    Prints start game/instructions message and validates input for game to start.
-    """
-    user_choice = input("Type 'p' to start game or 'r' to read the rules!\n>")
-    if user_choice == "r":
-        display_instructions()
-    elif user_choice == "p":
-        set_board(hit,miss,comp)
-
-def display_instructions():
-    """
-    Prints instructions to the screen for the user when "r" is selected.
-    """
-    print("These are the rules of my game")
+    return shot
 
 
 
+def show_board(hit,miss,comp):
 
-def set_board(hit,miss,comp):
-    """
-    Prints X- axis as 1-5 and Y-axis as A-E
-
-    """
     print("     0  1  2  3  4  5  6  7  8  9")
 
     place = 0
@@ -63,8 +40,42 @@ def set_board(hit,miss,comp):
             place = place + 1
         print(x," ",row)
    
-hit = [21,22]
-miss = [20,24,12,13]
-comp = [23] 
 
-new_game()
+def check_shot(shot,boat1,boat2,hit,miss,comp):
+
+    if shot in boat1:
+        boat1.remove(shot)
+        if len(boat1) > 0:
+            hit.append(shot)
+        else:
+            comp.append(shot)      
+    elif shot in boat2:
+        boat2.remove(shot)
+        if len(boat2) > 0:
+            hit.append(shot)
+        else:
+            comp.append(shot)    
+
+    else:
+        miss.append(shot) 
+
+
+    return boat1,boat2,hit,miss,comp
+
+
+boat1 = [45,46,47]   
+boat2 = [6,16,26]
+hit = []
+miss = []
+comp = [] 
+
+for i in range(10):
+    guesses = hit + miss + comp
+    shot = get_shot(guesses)
+    boat1,boat2,hit,miss,comp = check_shot(shot,boat1,boat2,hit,miss,comp)
+    show_board(hit,miss,comp)
+
+    if len(boat1) < 1 and len(boat2) < 1:
+        print('you have won')
+
+print("finished")        
